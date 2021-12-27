ae4413e9e243bdcf497eaecce6b14a898b6f9339
evelynsba <evelynsba@gmail.com>
2021-12-27 15:43:34 -0300

    teste heroku

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const routes = require('./routes')
 const porta = process.env.PORT || 3000;
 
 routes(app)
+app.get("/", function(req,res){
+    res.send("Ola, estou rodando no heroku")
+});
 
 app.listen(porta, () => console.log(`servidor está rodando na porta ${porta}`))
 
